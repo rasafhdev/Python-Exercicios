0799c2abd07e010fc7a120071c3e7794e0dc3700
rasafhdev <139464196+rasafhdev@users.noreply.github.com>
2023-08-04 20:56:47 -0300

    Update exe071.py

--- exe071.py
+++ exe071.py
@@ -1,10 +1,9 @@
-
 soma1 = 0
 soma2 = 0
 cliente_autenticado = False
 total_disponivel = 100000
 
-# A primeira validação é ver se o CPF é contém 11 digitos, não pode conter letra(string)
+# A primeira validação é verificar se o CPF contém 11 dígitos numéricos
 while True:
     cpf = input('Digite seu CPF: ')
 
@@ -17,10 +16,7 @@ while True:
             soma1 += int(cpf[i]) * (10 - i)
 
         resto1 = 11 - (soma1 % 11)
-        if resto1 > 9:
-            primeiro_digito_verificador = 0
-        else:
-            primeiro_digito_verificador = resto1
+        primeiro_digito_verificador = 0 if resto1 > 9 else resto1
 
         # Verifica o segundo dígito verificador
         soma2 = 0
@@ -28,10 +24,7 @@ while True:
             soma2 += int(cpf[i]) * (11 - i)
 
         resto2 = 11 - (soma2 % 11)
-        if resto2 > 9:
-            segundo_digito_verificador = 0
-        else:
-            segundo_digito_verificador = resto2
+        segundo_digito_verificador = 0 if resto2 > 9 else resto2
 
         # Verifica se os dígitos verificadores são válidos
         if int(cpf[9]) == primeiro_digito_verificador and int(cpf[10]) == segundo_digito_verificador:
@@ -45,7 +38,20 @@ while True:
 while cliente_autenticado:
     valor_saque = float(input('Valor do saque: '))
     if valor_saque <= 0 or valor_saque > total_disponivel:
-        print("Valor invalido ou excede o limite disponível.")
+        print("Valor inválido ou excede o limite disponível.")
         continue
     else:
+        notas_disponiveis = [200, 100, 50, 20, 10, 5, 2, 1]
+        notas_count = {200: 0, 100: 0, 50: 0, 20: 0, 10: 0, 5: 0, 2: 0, 1: 0}
+
+        for nota in notas_disponiveis:
+            while valor_saque >= nota:
+                valor_saque -= nota
+                notas_count[nota] += 1
+
+        print('Notas a serem entregues:')
+        for nota, quantidade in notas_count.items():
+            if quantidade > 0:
+                print(f'Nota de {nota}: {quantidade}')
+
         break
